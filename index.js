d04ad9d1642767863292b1b32a78bb1ff655dde2
zengraphic <info@zengraphic.it>
2022-02-17 16:45:42 +0100

    retrieve index keyboard and links

--- index.js
+++ index.js
@@ -3,18 +3,55 @@ require("dotenv").config();
 const { Telegraf } = require("telegraf");
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const bot = new Telegraf(BOT_TOKEN);
+const sheetID = "19A7jfTKkZ-_66dQUKpH3tpNFzqBOEQXC2WtPrr_QbIY";
+  const googleKey = process.env.GOOGLE_API_KEY;
 const axios = require("axios");
 
 bot.start((ctx) => ctx.reply("Benvenuto!"));
 bot.command("link", (ctx) => {
-  const sheetID = "19A7jfTKkZ-_66dQUKpH3tpNFzqBOEQXC2WtPrr_QbIY";
-  const sheet = "link";
-  const googleKey = process.env.GOOGLE_API_KEY;
-  const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetID}/values/${sheet}?alt=json&key=${googleKey}`;
-  getlinks(url, ctx);
+  getlinksKeyboard('index', ctx);
 });
-
-const getlinks = (url, ctx) => {
+const getlinksKeyboard = (sheet, ctx) => {
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetID}/values/${sheet}?alt=json&key=${googleKey}`;
+  axios
+    .get(url)
+    .then((res) => {
+      const response = res.data.values;
+      const action = [
+        {
+          text: "chiudi",
+          callback_data: "close",
+        },
+      ];
+      ctx.deleteMessage();
+      if (response && response.length) {
+        const linkButtons = response.map(link => [{
+          text: `${link}`,
+          callback_data: `${link}`
+        }]);
+        const keyboard = [...linkButtons,action];
+        bot.telegram.sendMessage(ctx.chat.id, "Che cateegoria vuoi visualizzare?", {
+          parse_mode: "Markdown",
+          reply_markup: {
+            inline_keyboard: keyboard,
+          },
+          disable_notification: true,
+        });
+        const callbacks = response.map(link => link[0]);
+        bot.action(callbacks, (ctx) => {
+          getlinks(ctx.match[0],ctx)
+        })
+      } else {
+        ctx.reply("riprova getlinksKeyboard no response");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      ctx.reply("riprova getlinksKeyboard");
+    });
+};
+const getlinks = (sheet, ctx) => {
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetID}/values/${sheet}?alt=json&key=${googleKey}`;
   axios
     .get(url)
     .then((res) => {
@@ -32,6 +69,10 @@ const getlinks = (url, ctx) => {
       ]);
       const action = [
         {
+          text: "indietro",
+          callback_data: "link",
+        },
+        {
           text: "chiudi",
           callback_data: "close",
         },
@@ -47,7 +88,7 @@ const getlinks = (url, ctx) => {
     })
     .catch((err) => {
       console.log(err);
-      ctx.reply("riprova");
+      ctx.reply("riprova getlinks");
     });
 };
 const convertToObj = (keys, valuesArrays) => {
@@ -58,5 +99,6 @@ const convertToObj = (keys, valuesArrays) => {
   return finalArrray;
 };
 bot.action("close", (ctx) => ctx.deleteMessage());
+bot.action("link", (ctx) => {getlinksKeyboard('index', ctx)});
 
 bot.launch();
